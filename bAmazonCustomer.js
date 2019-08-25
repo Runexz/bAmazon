7b8bfd206203e83520d8b4bc4b1ce0b8d17e293e
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-08-25 15:59:54 -0500

    created addToCart function

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -19,18 +19,46 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("Welcome to bAmazon store # " + connection.threadId);
     console.log("Please browse our wares!");
+    // addToCart();
+    browse();
+});
 
+var browse = function() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) 
+    })
+}
+
+var addToCart = function () {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (res);
         console.log(res);
-        for (let i = 0; i < res.length; i++) {
-            console.log(res[i].product_name)
-            
-        }
-        connection.end();
-        // addToCart();
-    })
-});
+        // for (let i = 0; i < res.length; i++) {
+        //     console.log(res[i].product_name)
+
+        // }
+        inquirer.prompt({
+            name: "choice",
+            type: "rawlist",
+            choices: function (value) {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].product_name);
+                }
+                return choiceArray;
+            },
+            message: "What product would you like to buy? (Keep in mind the stock_quantity)"
+
+            // })
+        }).then(function (answer) {
+
+        });
+    });
+}
+
+
+
+
 
 // var addToCart = function () {
 //     connection.query("SELECT * FROM products", function (err, res) {
