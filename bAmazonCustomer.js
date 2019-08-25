6eec01d041465a3b843facbcd6b073dbdeacf07f
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-08-25 13:51:24 -0500

    created connection js to sql

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -1,2 +1,27 @@
-var msql = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bAmazon_DB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw (res);
+        console.log(res);
+        connection.end();
+    })
+  });
