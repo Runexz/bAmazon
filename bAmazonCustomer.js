b80ceab8b338370b8b3628de754d32e7cd7b0072
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-08-26 20:10:05 -0500

    added comments

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -1,6 +1,9 @@
+//connection to NPM mysql 
 var mysql = require('mysql');
+//connection to NPM inquirer
 var inquirer = require('inquirer');
 
+//create a connection to MySQL database bAmazon_DB
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -15,39 +18,50 @@ var connection = mysql.createConnection({
     database: "bAmazon_DB"
 });
 
+//connect to MySQL database bAmazon_DB
 connection.connect(function (err) {
     if (err) throw err;
+    //displays the connection thread id with a welcome entrance
     console.log("Welcome to bAmazon store # " + connection.threadId);
     console.log("Please browse our wares!");
-    // addToCart();
+    //runs function browse after connection is made to bAmazon_DB
     browse();
 });
 
-var browse = function() {
+//created function browse to see if user wants to buy or not
+var browse = function () {
+    //connect and query bAmazon_DB
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (res);
+        //displays a response from table products in bAmazon_DB
         console.log(res)
+        //a prompt to ask the user with a question and displays 2 options
         inquirer.prompt({
             name: "buyOrNot",
             type: "rawlist",
             message: "Would you like to make a purchase?",
             choices: ["BUY", "NOT TODAY PAL"]
-        }).then(function(answer){
-            if(answer.buyOrNot.toUpperCase()=="BUY"){
+        }).then(function (answer) {
+            if (answer.buyOrNot.toUpperCase() == "BUY") {
+                //if they choose to BUY then function addToCart will run
                 addToCart();
             } else {
+                //If they do not want to buy the function keepShopping will run
                 keepShopping();
             }
         })
     })
 }
 
+//creates function addToCart that the user will select a product and quantity amount
 var addToCart = function () {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (res);
+        //a prompt that asks the user a question and displays a list of products
         inquirer.prompt({
             name: "choice",
             type: "rawlist",
+            //for loop used to cycle through products and display them in choices
             choices: function (value) {
                 var choiceArray = [];
                 for (var i = 0; i < res.length; i++) {
@@ -56,37 +70,38 @@ var addToCart = function () {
                 return choiceArray;
             },
             message: "What product would you like to buy? (Keep in mind the stock_quantity)"
-
-            // })
+            //uses the choice made and matches the database and makes another prompt about quantity
         }).then(function (answer) {
-            for (var i = 0; i < res.length; i ++) {
+            for (var i = 0; i < res.length; i++) {
                 if (res[i].product_name == answer.choice) {
-                    var chosenProduct = res [i];
+                    var chosenProduct = res[i];
                     inquirer.prompt({
                         name: "amount",
                         type: "input",
                         message: "Quantity amount?",
-                        validate: function(value) {
-                            if(isNaN(value) == false) {
+                        //this makes sure the input is a number and not a letter
+                        validate: function (value) {
+                            if (isNaN(value) == false) {
                                 return true;
                             } else {
                                 return false;
                             }
                         }
-                    }).then(function(answer) {
+                        //the input will match the database amount
+                    }).then(function (answer) {
                         if (chosenProduct.stock_quantity == 0) {
                             console.log("Sorry we are out of stock of that item. Please check back tomorrow");
                             keepShopping();
                         } else {
-                            connection.query("UPDATE products SET ? WHERE ?",[{
+                            connection.query("UPDATE products SET ? WHERE ?", [{
                                 stock_quantity: answer.amount
                             }, {
-                                id:chosenProduct.id
-                            }], function (err,res){
+                                id: chosenProduct.id
+                            }], function (err, res) {
                                 console.log("Your order has been placed!");
                                 keepShopping();
                             })
-                           
+
                         }
                     })
                 }
@@ -96,17 +111,20 @@ var addToCart = function () {
     });
 }
 
-
-var keepShopping = function() {
+//create a function keepShopping that will ask if the user wants to continue shopping or leave
+var keepShopping = function () {
+    //a prompt to ask a question with 2 choices
     inquirer.prompt({
         name: "continue",
         type: "rawlist",
         message: "Would you like to keep shopping?",
         choices: ["CONTINUE SHOPPING", "LEAVE"]
-    }).then(function(answer){
-        if(answer.continue.toUpperCase()=="LEAVE") {
+        //the answer from choices will be used for below
+    }).then(function (answer) {
+        if (answer.continue.toUpperCase() == "LEAVE") {
             console.log("I understand. Please return when you have more money.")
         } else {
+            //if the user wants to continue shopping the function browse will begin
             browse();
         }
     })
